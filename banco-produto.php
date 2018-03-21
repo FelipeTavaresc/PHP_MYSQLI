dec357b1f51f87d914f434afd998f17557abfe47
Felipe Tavares <felipe.tavares@outlook.com>
2018-03-21 15:17:35 -0300

    Relacionando produtos com categoriais

--- banco-produto.php
+++ banco-produto.php
@@ -3,7 +3,8 @@
 function listaProdutos($conexao){
 	$produtos = array();
 
-	$resultado = mysqli_query($conexao, "SELECT * FROM produtos ORDER BY nome");
+	$resultado = mysqli_query($conexao, "select p.*, c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id = c.id");
+
 	while ($produto = mysqli_fetch_assoc($resultado)) {
 		array_push($produtos, $produto);
 	}
@@ -12,16 +13,14 @@ function listaProdutos($conexao){
 
 }
 
-function insereProduto($conexao, $nome, $preco, $descricao){
-	$query = "INSERT INTO produtos (nome, preco, descricao) VALUES ('{$nome}' , {$preco}, '{$descricao}')";	
+function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id){
+	$query = "INSERT INTO produtos (nome, preco, descricao, categoria_id) VALUES ('{$nome}' , {$preco}, '{$descricao}' , '{$categoria_id}')";	
 	return mysqli_query($conexao, $query);
-
 }
 
 function removeProduto($conexao, $id){
 	$query = "DELETE FROM produtos WHERE id = {$id}";
 	return mysqli_query($conexao, $query);
 }
-
 
 ?>
